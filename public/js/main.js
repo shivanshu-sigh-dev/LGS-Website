404ab769c8dcebf69a8fff03217ae6006eb2b524
Shaik-Sami <shaiksami404@gmail.com>
2021-06-04 22:56:13 +0530

    updated some pages and carousel

--- public/js/main.js
+++ public/js/main.js
@@ -220,6 +220,18 @@ const sendQuotation = event => {
 })();
 
 /**
+   * Hero carousel indicators
+   */
+  let heroCarouselIndicators = select("#hero-carousel-indicators")
+  let heroCarouselItems = select('#heroCarousel .carousel-item', true)
+
+  heroCarouselItems.forEach((item, index) => {
+    (index === 0) ?
+    heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "' class='active'></li>":
+      heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "'></li>"
+  });
+
+/**
    * Clients Slider
    */
   new Swiper('.clients-slider', {
