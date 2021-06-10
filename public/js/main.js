033c1a47a0f920a62003ad6f746e366233c032dc
Shivanshu Singh <shivanshu.singh2102@gmail.com>
2021-06-10 13:01:12 +0530

    Footer is improved and code indentation is applied

--- public/js/main.js
+++ public/js/main.js
@@ -217,19 +217,19 @@ const sendQuotation = event => {
     }
   });
 
-})();
-
-/**
+  /**
    * Hero carousel indicators
    */
-  let heroCarouselIndicators = select("#hero-carousel-indicators")
-  let heroCarouselItems = select('#heroCarousel .carousel-item', true)
+   let heroCarouselIndicators = select("#hero-carousel-indicators")
+   let heroCarouselItems = select('#heroCarousel .carousel-item', true)
+ 
+   heroCarouselItems.forEach((item, index) => {
+     (index === 0) ?
+     heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "' class='active'></li>":
+       heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "'></li>"
+   }); 
 
-  heroCarouselItems.forEach((item, index) => {
-    (index === 0) ?
-    heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "' class='active'></li>":
-      heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "'></li>"
-  });
+})();
 
 /**
    * Clients Slider
